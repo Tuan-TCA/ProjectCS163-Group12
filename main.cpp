29d8f05f3d703f3a43111cebaa25efe899ce27d3
Tuan-TCA <tuan06nbk@gmail.com>
2025-03-08 11:32:35 +0700

    Code mẫu set up của RayLib
    Để chạy hãy tải thư mục AppRaylib về (chứa 3 file CMakeLists.txt, main.cpp, run.bat)
    - Mở thư mục vừa tải trong VScode
    - Nhập vào terminal lệnh sau: .\run.bat

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include<raylib.h>
+#include<iostream>
+
+
+using namespace std;
+
+
+int main() {
+    InitWindow(900, 900, "Hello Raylib");
+    CloseWindow();
+    cout<<"323";
+    return 0;
+}
