fe962754175d840cf38e153943d8ef49bfe3099d
Khang Thinh <khangthinh111555@gmail.com>
2025-03-13 23:39:34 +0700

    add ButtonFromImage class for image-based buttons

--- include/Button.h
+++ include/Button.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "raylib.h"
-
+#include <string>
+#include "Variables.h"
 class MyRec {
 private:
     Rectangle bounds;
@@ -45,28 +46,56 @@ public:
         textColor = textCol;
         isHovered = false;
     }
-        
 
     void Update() {
         Vector2 mousePoint = GetMousePosition();
         isHovered = CheckCollisionPointRec(mousePoint, bounds);
     }
 
-    void Draw() {
+    virtual void Draw() {
         Update();
         DrawRectangleRec(bounds, isHovered ? hoverColor : color);
         int textWidth = MeasureText(label, 20);
         DrawText(label, bounds.x + (bounds.width - textWidth) / 2, bounds.y + (bounds.height - 20) / 2, 20, textColor);
     }
 
-    void DrawText2D (string link) {
+    void DrawText2D (std::string link) {
         Texture2D image = LoadTexture(link.c_str());
         Rectangle sourceRec = {0,0, (float) image.width, (float) image.height};
-        Rectangle destRec = {}
-        DrawText
+        Rectangle destRec;
     }
 
+    Rectangle getBound(){
+        return bounds;
+    }
+    bool isOver(){
+        return isHovered;
+    }
     bool IsClicked() {
         return isHovered && IsMouseButtonPressed(MOUSE_LEFT_BUTTON);
     }
 };
+
+
+class ButtonFromImage : public Button {
+private:
+    Texture2D texture;
+    Texture2D textureOver;
+public:
+    ButtonFromImage() : Button(0, 0, 0, 0, "", RAYWHITE, RAYWHITE, RAYWHITE), texture({ 0 }), textureOver({ 0 }) {}
+    ButtonFromImage(const char* imagePath, const char* imageOverPath, float x, float y, float width, float height)
+        : Button(x, y, width, height, "", RAYWHITE, RAYWHITE, RAYWHITE) {
+        texture = resizedImage(imagePath, width, height);
+        textureOver = resizedImage(imageOverPath, width, height);
+                if (texture.id == 0 || textureOver.id == 0){
+            TraceLog(LOG_ERROR, "Failed to load image texture.");
+        }
+    }
+
+    void Draw() override {
+        Update();
+        Rectangle bound = getBound();
+        DrawTexture(!isOver() ? texture : textureOver, bound.x, bound.y, WHITE);
+    }
+};
+
